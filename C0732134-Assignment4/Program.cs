c7e449a3782ae263008010e9d192cbe9b6d5a264
hps3103 <ssingh.harpushpam@gmail.com>
2019-04-05 18:06:49 -0400

    Make the Sample Code working !

--- C0732134-Assignment4/Program.cs
+++ C0732134-Assignment4/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,56 @@ namespace C0732134_Assignment4
 {
     class Program
     {
+        ArrayList Beowulf;
+
         static void Main(string[] args)
         {
 
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
         }
+
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+
+
+        public void ReadTextFiles()
+        {
+            // Read file using StreamReader. Read file line by line
+            using (StreamReader file = new StreamReader("c:/area51/beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+
+
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            // https://stackoverflow.com/questions/17812566/count-words-and-spaces-in-s
+
+            int countLetters = 0;
+            int countSpaces = 0;
+
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c))
+                    countLetters++;
+                if (char.IsWhiteSpace(c))
+                    countSpaces++;
+            }
+            return countSpaces;
+        }
+        
     }
 }
